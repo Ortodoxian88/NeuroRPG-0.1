95b8347f8e5c0540fecdf6e688c3149ee427b1a7
Dmitry Dobronravov <ortonrav@gmail.com>
2026-04-17 09:29:10 +0000

    Delete src directory

--- src/components/ui/Skeleton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { cn } from '@/src/lib/utils';
-
-export const Skeleton = ({ className }: { className?: string }) => {
-  return (
-    <div className={cn("animate-pulse bg-neutral-800/50 rounded-lg", className)} />
-  );
-};
